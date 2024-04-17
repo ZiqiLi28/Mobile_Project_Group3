fb26373dd698ec3afb478cded1209f082ea997b6
Lufei Wu <lufiewu@outlook.com>
2024-04-17 14:53:05 +0300

    SigninSignup

---
+++ app/src/main/java/com/example/accounting/SigninActivity.kt
@@ -0,0 +1,58 @@
+package com.example.accounting
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+
+import androidx.appcompat.app.AppCompatActivity
+import com.example.accounting.databinding.ActivitySigninBinding
+import com.google.firebase.auth.FirebaseAuth
+
+
+class SigninActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySigninBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySigninBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+        binding.textView.setOnClickListener{
+            val intent = Intent(this, SignupActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.button.setOnClickListener{
+            val email = binding.emailEt.text.toString()
+            val pass = binding.passET.text.toString()
+
+            if(email.isNotEmpty() && pass.isNotEmpty()){
+
+                    firebaseAuth.signInWithEmailAndPassword(email, pass).addOnCompleteListener{
+                        if(it.isSuccessful){
+                            val email = binding.emailEt.text.toString() // Assuming you're using email for username
+                            val intent = Intent(this, ProfileActivity::class.java)
+                            intent.putExtra("email", email)
+                            startActivity(intent)
+
+
+
+                        }else{
+                            Toast.makeText(this,it.exception.toString(), Toast.LENGTH_SHORT).show()
+                        }
+                    }
+            } else {
+                Toast.makeText(this,"Empty Fields are not allowed", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        binding.searchIvBack.setOnClickListener {
+            finish()
+        }
+    }
+
+
+}
