a913a663bb07a034d4852ec739917f989e392277
ZiqiLi28 <143429680+ZiqiLi28@users.noreply.github.com>
2024-04-28 15:12:40 +0300

    Update DBManager.kt

--- app/src/main/java/com/example/accounting/db/DBManager.kt
+++ app/src/main/java/com/example/accounting/db/DBManager.kt
@@ -5,6 +5,7 @@ import android.content.ContentValues
 import android.content.Context
 import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
+import com.example.accounting.utils.FloatUtils
 
 class DBManager {
     companion object {
